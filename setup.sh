64b1938e5f80ed2ff259f8640d5a2ae959819adc
Frode Oijord <foijord@gmail.com>
2017-11-20 10:09:40 +0000

    update repository after adding docker

--- setup.sh
+++ setup.sh
@@ -14,5 +14,6 @@ sudo apt-get install -y \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+sudo apt-get update
 
 sudo apt-get install -y docker-ce
