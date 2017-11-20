cf1e12059705d86143acbe2cec26875ac0dce717
Frode Oijord <foijord@gmail.com>
2017-11-20 10:03:52 +0000

    install docker

--- setup.sh
+++ setup.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
-sudo apt-get install emacs24
+sudo apt-get update
+sudo apt-get install -y emacs24
+
+
+# install docker
+sudo apt-get install -y \
+     emacs24 \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+
+sudo apt-get install -y docker-ce
