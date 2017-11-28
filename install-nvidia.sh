2a8584d4c59eb7a83595f1878c51c9f097ba6555
Frode Oijord <foijord@gmail.com>
2017-11-28 13:17:23 +0000

    run as sudo

--- install-nvidia.sh
+++ install-nvidia.sh
@@ -6,8 +6,8 @@ sudo apt-get update && sudo apt-get install -y nvidia-375
 
 # run in text mode (not graphical)
 sudo apt-get install -y xorg
-echo 'manual' | dd of=/etc/init/lightdm.override
-systemctl set-default multi-user.target
-nvidia-xconfig -a --use-display-device=None --virtual=4096x2160
+echo 'manual' | sudo dd of=/etc/init/lightdm.override
+sudo systemctl set-default multi-user.target
+sudo nvidia-xconfig -a --use-display-device=None --virtual=4096x2160
 
 export DISPLAY=:0
