14ef9d59b62bf80e7aad03477cf14d2773433bda
Frode Oijord <foijord@gmail.com>
2017-11-28 11:45:03 +0000

    configure X

--- install-nvidia.sh
+++ install-nvidia.sh
@@ -2,5 +2,12 @@
 
 # install NVIDIA drivers
 sudo apt-add-repository -y ppa:graphics-drivers/ppa
-sudo apt-get update && apt-get install -y nvidia-375
+sudo apt-get update && sudo apt-get install -y nvidia-375
 
+# run in text mode (not graphical)
+sudo apt-get install -y xorg
+echo 'manual' | dd of=/etc/init/lightdm.override
+systemctl set-default multi-user.target
+nvidia-xconfig -a --use-display-device=None --virtual=4096x2160
+
+export DISPLAY=:0
