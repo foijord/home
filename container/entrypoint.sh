07db8ef3b68ebd9fa124d42e94dfb4f54fcccd58
Frode Oijord <FOeijord@slb.com>
2018-01-05 09:34:27 +0100

    removed container directory

--- container/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# run one x session in text mode (not graphical)
-xinit -- :0 -nolisten tcp vt8 -noreset +extension GLX +extension RANDR +extension RENDER +extension XFIXES &
-
-exec "$@"
